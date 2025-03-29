bd2d1e24def68a5c0cd9e0efadd9d19c03be6838
dragosjon <107347132+dragosjon@users.noreply.github.com>
2025-03-29 12:49:42 +0200

    pp

--- Ion_Dragos_ActivitateSDD2025/Produs(listaSimpla) - eu.c
+++ Ion_Dragos_ActivitateSDD2025/Produs(listaSimpla) - eu.c
@@ -4,5 +4,90 @@
 #include <string.h>
 
 typedef struct {
+	int cod;
+	char* denumire;
+	float pret;
+}Produs;
 
-};
+typedef struct {
+	Produs inf;
+	struct nodLS* next;
+}nodLS;
+
+void inserareNod(nodLS** cap, Produs p) {
+	nodLS* nou = (nodLS*)malloc(sizeof(nodLS));
+
+	nodLS* cap = NULL;
+
+	nou->inf.cod = p.cod;
+	
+	nou->inf.denumire = (char*)malloc((strlen(p.denumire) + 1) * sizeof(char));
+	strcpy(nou->inf.denumire, p.denumire);
+
+	nou->inf.pret = p.pret;
+
+	if (*cap == NULL) {
+		*cap = nou;
+	}
+	else
+	{
+		nodLS* temp = *cap;
+		while (temp->next) {
+			temp = temp->next;
+		}
+		temp->next = nou;
+	}
+}
+
+void traversareLista(nodLS* cap) {
+	nodLS* temp = cap;
+	while (temp) {
+		printf("Cod = %d, Denumire = %s, Pret = %.2f",
+			temp->inf.cod, temp->inf.denumire, temp->inf.pret);
+		printf("/n");
+		temp = temp->next;
+	}
+
+}
+
+Produs citireProdus(FILE* file) {
+	Produs p;
+	char buffer[100];
+
+	fscanf(file, "%d", p.cod);
+
+	fscanf(file, "%s", &p.denumire);
+	p.denumire = (char*)malloc((strlen(buffer) + 1) * sizeof(char));
+	strcpy(p.denumire, buffer);
+
+	fscanf(file, "%.2f", &p.pret);
+
+	return p;
+}
+
+void dezalocareLista(nodLS* cap) {
+	nodLS* temp = cap;
+	while (temp) {
+		nodLS* aux = temp->next;
+		free(temp->inf.denumire);
+		free(temp);
+		temp = aux;
+	}
+}
+
+int main() {
+	int n = 3;
+	nodLS* cap = NULL;
+
+	FILE* file = fopen("produs.txt", "r");
+
+	Produs p;
+	for (int i = 0; i < n; i++){
+		p = citireProdus(file);
+		inserareNod(&cap, p);
+		free(p.denumire);
+	}
+
+	traversareLista(cap);
+	dezalocareLista(cap);
+}
